9abbe21d2a8ff9e8d1b974b1ec70f739a5c5614c
Yury Shakhau <shakhauyury@gmail.com>
2026-08-06 10:30:29 +0300

    Empty project created

---
+++ src/main/java/by/shakhau/ps/product/PaymentSystemProductServiceApplication.java
@@ -0,0 +1,13 @@
+package by.shakhau.ps.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSystemProductServiceApplication.class, args);
+    }
+
+}
